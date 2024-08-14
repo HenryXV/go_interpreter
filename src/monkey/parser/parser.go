08db450bd164733465fdf0f0e81f96f7b2adf112
henry <henri.r.s@hotmail.com>
2024-08-13 23:33:21 -0300

    feat: fleshing out the ast and parser

---
+++ src/monkey/parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"go_interpreter/src/monkey/ast"
+	"go_interpreter/src/monkey/lexer"
+	"go_interpreter/src/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	currentToken token.Token
+	peekToken    token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	p.nextToken()
+	p.nextToken()
+
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.currentToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
